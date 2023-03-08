c046dd136f806ad48c589d07b95d1dafbde53586
BonesetterB <122158929+BonesetterB@users.noreply.github.com>
2023-03-08 08:37:21 +0200

    update

--- hmw.py
+++ hmw.py
@@ -1,5 +1,4 @@
 from datetime import datetime
-days=['Monday','Tuesday','Wednesday','Thursday','Friday']
 def get_str_date(date):
     x=str(date).split(' ')
     c=x[0].split('-')
@@ -7,27 +6,29 @@ def get_str_date(date):
     return string_data.strftime('%A')
 
 def get_birthdays_per_week(users):
+    days=['Monday','Tuesday','Wednesday','Thursday','Friday']
     b=[{"Monday":[]},
     {"Tuesday":[]},
     {"Wednesday":[]},
     {"Thursday":[]},
-    {"Friday":[]}]
+    {"Friday":[]},
+    ]
     lst_days=[]
 
     for i in users:
         for k,v in i.items():
             day=get_str_date(v)
-            if day in days:
-                for c in days:
-                    if c==day:
-                        for t in b:
-                            for n,l in t.items():
-                                if n==c:
-                                        l.append(k)
-                                    
-                        break
-            else:
-                b['Monday'].append(k)
+            yes=False
+            for t in b:
+                    for n,l in t.items():
+                            if n==day:
+                                l.append(k)
+                                yes=True
+                            elif day=='Sunday' or day=='Saturday':
+                                l.append(k)
+                                yes=True
+                    if yes:
+                         break
     for j in b:
         for keys, value in j.items():
             if len(value)==0:
@@ -37,5 +38,6 @@ def get_birthdays_per_week(users):
                 lst_days.append(f'{keys}: {a}')
 
     return  '\n'.join(lst_days)
-print(get_birthdays_per_week([{'Illy':datetime(year=2023, month=1, day=19)},{'Mia':datetime(year=2023, month=1, day=19)},{'Nina':datetime(year=2028, month=10, day=9)}]))
+if __name__ == "__main__" :
+    print(get_birthdays_per_week([{'Illy':datetime(year=2023, month=1, day=21)},{'Mia':datetime(year=2023, month=1, day=19)},{'Nina':datetime(year=2028, month=10, day=9)}]))
                                 
